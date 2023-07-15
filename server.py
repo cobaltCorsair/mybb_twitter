1d7daaf7470d09101d75785a0df551b5ef0bfb1e
cobaltCorsair <cobaltcorsair@yandex.ru>
2023-07-16 02:51:08 +0300

    refactoring

--- server.py
+++ server.py
@@ -21,29 +21,32 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 import views
 views.socketio = socketio
 
-app.add_url_rule('/check_user', view_func=UserView.as_view('check_user', socketio=socketio), methods=['POST'])
-app.add_url_rule('/create_message', view_func=CreateMessageView.as_view('create_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/delete_message', view_func=DeleteMessageView.as_view('delete_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/update_message', view_func=UpdateMessageView.as_view('update_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/ban_user/<int:user_id>', view_func=BanUserView.as_view('ban_user', socketio=socketio), methods=['POST'])
-app.add_url_rule('/unban_user/<int:user_id>', view_func=UnbanUserView.as_view('unban_user', socketio=socketio), methods=['POST'])
-app.add_url_rule('/create_comment', view_func=CreateCommentView.as_view('create_comment', socketio=socketio), methods=['POST'])
-app.add_url_rule('/delete_comment', view_func=DeleteCommentView.as_view('delete_comment', socketio=socketio), methods=['POST'])
-app.add_url_rule('/like_message', view_func=LikeMessageView.as_view('like_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/remove_like_message',
-                 view_func=RemoveLikeMessageView.as_view('remove_like_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/get_message_likes/<string:message_id>',
-                 view_func=GetMessageLikesView.as_view('get_message_likes'), methods=['GET'])
-app.add_url_rule('/ignore_user', view_func=IgnoreUserView.as_view('ignore_user', socketio=socketio), methods=['POST'])
-app.add_url_rule('/unignore_user', view_func=UnignoreUserView.as_view('unignore_user', socketio=socketio), methods=['POST'])
-app.add_url_rule('/get_ignored_users/<int:user_id>', view_func=GetIgnoredUsersView.as_view('get_ignored_users'), methods=['GET'])
-app.add_url_rule('/report_message', view_func=ReportMessageView.as_view('report_message', socketio=socketio), methods=['POST'])
-app.add_url_rule('/report_comment', view_func=ReportCommentView.as_view('report_comment', socketio=socketio), methods=['POST'])
-app.add_url_rule('/get_top_users', view_func=GetTopUsersView.as_view('get_top_users'), methods=['GET'])
-app.add_url_rule('/get_recent_messages', view_func=GetRecentMessagesView.as_view('get_recent_messages'), methods=['GET'])
-app.add_url_rule('/send_notification', view_func=SendNotificationView.as_view('send_notification', socketio=socketio), methods=['POST'])
-app.add_url_rule('/get_message_comments/<string:message_id>', view_func=GetMessageCommentsView.as_view('get_message_comments'), methods=['GET'])
-app.add_url_rule('/get_user_posts/<int:user_id>', view_func=GetUserPostsView.as_view('get_user_posts'), methods=['GET'])
+url_rules = [
+    ("/check_user", UserView.as_view('check_user', socketio=socketio), ['POST']),
+    ("/create_message", CreateMessageView.as_view('create_message', socketio=socketio), ['POST']),
+    ("/delete_message", DeleteMessageView.as_view('delete_message', socketio=socketio), ['POST']),
+    ("/update_message", UpdateMessageView.as_view('update_message', socketio=socketio), ['POST']),
+    ("/ban_user/<int:user_id>", BanUserView.as_view('ban_user', socketio=socketio), ['POST']),
+    ("/unban_user/<int:user_id>", UnbanUserView.as_view('unban_user', socketio=socketio), ['POST']),
+    ("/create_comment", CreateCommentView.as_view('create_comment', socketio=socketio), ['POST']),
+    ("/delete_comment", DeleteCommentView.as_view('delete_comment', socketio=socketio), ['POST']),
+    ("/like_message", LikeMessageView.as_view('like_message', socketio=socketio), ['POST']),
+    ("/remove_like_message", RemoveLikeMessageView.as_view('remove_like_message', socketio=socketio), ['POST']),
+    ("/get_message_likes/<string:message_id>", GetMessageLikesView.as_view('get_message_likes'), ['GET']),
+    ("/ignore_user", IgnoreUserView.as_view('ignore_user', socketio=socketio), ['POST']),
+    ("/unignore_user", UnignoreUserView.as_view('unignore_user', socketio=socketio), ['POST']),
+    ("/get_ignored_users/<int:user_id>", GetIgnoredUsersView.as_view('get_ignored_users'), ['GET']),
+    ("/report_message", ReportMessageView.as_view('report_message', socketio=socketio), ['POST']),
+    ("/report_comment", ReportCommentView.as_view('report_comment', socketio=socketio), ['POST']),
+    ("/get_top_users", GetTopUsersView.as_view('get_top_users'), ['GET']),
+    ("/get_recent_messages", GetRecentMessagesView.as_view('get_recent_messages'), ['GET']),
+    ("/send_notification", SendNotificationView.as_view('send_notification', socketio=socketio), ['POST']),
+    ("/get_message_comments/<string:message_id>", GetMessageCommentsView.as_view('get_message_comments'), ['GET']),
+    ("/get_user_posts/<int:user_id>", GetUserPostsView.as_view('get_user_posts'), ['GET'])
+]
+
+for url_rule, view, methods in url_rules:
+    app.add_url_rule(url_rule, view_func=view, methods=methods)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
