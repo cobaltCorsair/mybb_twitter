b7dad222580bfdb340fec6f05a89d0989039b71a
cobaltCorsair <cobaltcorsair@yandex.ru>
2023-08-06 21:36:31 +0300

    Update client

--- client/script.js
+++ client/script.js
@@ -6,12 +6,11 @@ const socket = io.connect('http://localhost:5000');
 // ================================
 // SOCKET CONNECTION HANDLERS
 // ================================
-socket.on('connect', function() {
+socket.on('connect', () => {
     console.log('Connected to the server');
     socket.emit('join', { room: 'room' });
 });
-
-socket.on('new tweet', function(data) {
+socket.on('new tweet', data => {
     console.log("Received new tweet event:", data);
     const tweetsWrapper = document.getElementById('tweets-wrapper');
     const newTweetElement = document.createElement('div');
@@ -26,13 +25,9 @@ socket.on('new tweet', function(data) {
             <div class="tweet-time-date">
                 <span class="tweet-time">Текущее время</span> · <span class="tweet-date">Текущая дата</span>
             </div>
-            <div class="tweet-actions">
-                <!-- Additional tweet action buttons can be added here -->
-            </div>
+            <div class="tweet-actions"></div>
         </div>
-        <div class="comments">
-            <!-- Comments for the tweet can be added here -->
-        </div>
+        <div class="comments"></div>
     `;
     tweetsWrapper.insertBefore(newTweetElement, tweetsWrapper.firstChild);
 });
@@ -40,103 +35,78 @@ socket.on('new tweet', function(data) {
 // ================================
 // UI FUNCTIONS
 // ================================
-function toggleNav() {
-    var sidebar = document.getElementById("mySidebar");
-    var button = document.querySelector(".openbtn");
-    var userContainer = document.getElementById("userContainer");
-    var blockedUsersButton = document.querySelector(".blocked-users");
-    if (sidebar.style.width === "300px") {
-        sidebar.style.width = "0";
-        button.style.right = "-50px";
-        userContainer.classList.add("closed");
-        blockedUsersButton.style.display = "none";
-    } else {
-        sidebar.style.width = "300px";
-        button.style.right = "250px";
-        userContainer.classList.remove("closed");
-        blockedUsersButton.style.display = "flex";
-    }
-}
+const toggleNav = () => {
+    const sidebar = document.getElementById("mySidebar");
+    const button = document.querySelector(".openbtn");
+    const userContainer = document.getElementById("userContainer");
+    const blockedUsersButton = document.querySelector(".blocked-users");
 
-function updateCounter() {
-    var tweetInput = document.getElementById("tweetInput");
-    var tweetCounter = document.getElementById("tweetCounter");
-    if (tweetInput.value.length > 500) {
+    const isSidebarOpen = sidebar.style.width === "300px";
+    sidebar.style.width = isSidebarOpen ? "0" : "300px";
+    button.style.right = isSidebarOpen ? "-50px" : "250px";
+    userContainer.classList[isSidebarOpen ? 'add' : 'remove']("closed");
+    blockedUsersButton.style.display = isSidebarOpen ? "none" : "flex";
+}
+const updateCounter = () => {
+    const tweetInput = document.getElementById("tweetInput");
+    const tweetCounter = document.getElementById("tweetCounter");
+    const tweetLength = tweetInput.value.length;
+
+    if (tweetLength > 500) {
         tweetInput.value = tweetInput.value.substring(0, 500);
     }
-    tweetCounter.textContent = tweetInput.value.length + "/500";
-    tweetCounter.style.color = tweetInput.value.length === 500 ? "red" : "#888";
-}
 
-function toggleUserInfo() {
-    var userInfo = document.getElementById("userInfo");
+    tweetCounter.textContent = `${tweetLength}/500`;
+    tweetCounter.style.color = tweetLength === 500 ? "red" : "#888";
+}
+const toggleUserInfo = () => {
+    const userInfo = document.getElementById("userInfo");
     userInfo.classList.toggle("minimized");
 }
-
-function toggleNotifications() {
-    var notificationPanel = document.getElementById("notificationPanel");
-    notificationPanel.style.display = notificationPanel.style.display === "block" ? "none" : "block";
+const toggleElementVisibility = elementId => {
+    const element = document.getElementById(elementId);
+    element.style.display = element.style.display === "block" ? "none" : "block";
 }
-
-function clearNotifications() {
-    var notificationPanel = document.getElementById("notificationPanel");
+const toggleNotifications = () => toggleElementVisibility("notificationPanel");
+const toggleBlockedUsers = () => toggleElementVisibility("blockedUsersPanel");
+const clearNotifications = () => {
+    const notificationPanel = document.getElementById("notificationPanel");
     notificationPanel.innerHTML = '<button class="clear-button" onclick="clearNotifications()">Clear All Notifications</button>';
     document.getElementById("notificationCount").textContent = "0";
 }
+const unblockUser = username => {
+    const blockedUsersPanel = document.getElementById("blockedUsersPanel");
+    const users = [...blockedUsersPanel.getElementsByClassName("blocked-user")];
 
-function toggleBlockedUsers() {
-    var blockedUsersPanel = document.getElementById("blockedUsersPanel");
-    blockedUsersPanel.style.display = blockedUsersPanel.style.display === "block" ? "none" : "block";
+    const userToUnblock = users.find(user => user.textContent.includes(username));
+    if (userToUnblock) userToUnblock.remove();
 }
-
-function unblockUser(username) {
-    var blockedUsersPanel = document.getElementById("blockedUsersPanel");
-    var users = blockedUsersPanel.getElementsByClassName("blocked-user");
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].textContent.includes(username)) {
-            users[i].remove();
-            break;
-        }
-    }
-}
-
-function toggleComments(button) {
+const toggleComments = button => {
     const commentsSection = button.closest('.tweet').nextElementSibling;
     commentsSection.style.display = commentsSection.style.display === 'none' ? 'block' : 'none';
     drawLineBetweenComments();
 }
-
-function toggleSubcomments(button) {
+const toggleSubcomments = button => {
     const subcommentsSection = button.closest('.comment').nextElementSibling;
     subcommentsSection.style.display = subcommentsSection.style.display === 'none' ? 'block' : 'none';
     drawLineBetweenComments();
 }
-
-function toggleLike(button) {
-    button.classList.toggle('liked');
-}
-
-function confirmDelete(button) {
-    if (confirm('Вы уверены, что хотите удалить этот элемент?')) {
-        deleteTweet(button);
+const toggleLike = button => button.classList.toggle('liked');
+const confirmAndExecute = (message, action, button) => {
+    if (confirm(message)) {
+        action(button);
     }
 }
-
-function confirmBlacklist(button) {
-    if (confirm('Вы уверены, что хотите добавить этого пользователя в черный список?')) {
-        addToBlacklist(button);
-    }
-}
-
-function drawLineBetweenComments() {
+const confirmDelete = button => confirmAndExecute('Вы уверены, что хотите удалить этот элемент?', deleteTweet, button);
+const confirmBlacklist = button => confirmAndExecute('Вы уверены, что хотите добавить этого пользователя в черный список?', addToBlacklist, button);
+const drawLineBetweenComments = () => {
     const commentsBlock = document.querySelector(".comments");
     if (!commentsBlock) return;
 
-    const comments = commentsBlock.querySelectorAll(".comment");
+    const comments = [...commentsBlock.querySelectorAll(".comment")];
     if (comments.length === 0) return;
 
-    const firstComment = comments[0];
-    const lastComment = comments[comments.length - 1];
+    const [firstComment, lastComment] = [comments[0], comments[comments.length - 1]];
 
     const topPosition = firstComment.offsetTop;
     const bottomPosition = lastComment.offsetTop + (lastComment.offsetHeight / 2);
@@ -144,13 +114,11 @@ function drawLineBetweenComments() {
     const lineHeight = bottomPosition - topPosition;
     commentsBlock.style.setProperty("--line-height", `${lineHeight}px`);
 }
-
-document.addEventListener("DOMContentLoaded", drawLineBetweenComments);
-document.querySelector(".comments").addEventListener("DOMSubtreeModified", drawLineBetweenComments);
-
-function sendTweet() {
+const sendTweet = () => {
     const tweetInput = document.getElementById("tweetInput");
     const tweetContent = tweetInput.value;
     socket.emit('create message', { content: tweetContent });
     tweetInput.value = '';
 }
+document.addEventListener("DOMContentLoaded", drawLineBetweenComments);
+document.querySelector(".comments").addEventListener("DOMSubtreeModified", drawLineBetweenComments);
